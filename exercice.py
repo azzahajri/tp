73721eeb78e17c57fe85b7503994e2cf42fd9459
azza <azzahajri70@gmail.com>
2023-11-30 16:04:15 +0100

    first commit

---
+++ exercice.py
@@ -0,0 +1,75 @@
+from ortools.sat.python import cp_model
+import time
+
+def generer_emploi_du_temps(enseignants, salles, cours_liste, horaires, jours):
+    model = cp_model.CpModel()
+
+    emploi_du_temps = {(ens, salle, cours, horaire, jour): model.NewBoolVar(f'{ens}_{salle}_{cours}_{horaire}_{jour}')
+                       for ens in enseignants
+                       for salle in salles
+                       for cours in cours_liste
+                       for horaire in horaires
+                       for jour in jours}
+
+    # Contraintes
+    for ens in enseignants:
+        for horaire in horaires:
+            for jour in jours:
+                model.Add(sum(emploi_du_temps[(ens, salle, cours, horaire, jour)] for salle in salles for cours in cours_liste) <= 1)
+
+    for salle in salles:
+        for horaire in horaires:
+            for jour in jours:
+                model.Add(sum(emploi_du_temps[(ens, salle, cours, horaire, jour)] for ens in enseignants for cours in cours_liste) <= 1)
+
+    for cours in cours_liste:
+        for horaire in horaires:
+            for jour in jours:
+                model.Add(sum(emploi_du_temps[(ens, salle, cours, horaire, jour)] for ens in enseignants for salle in salles) <= 1)
+
+    for cours in cours_liste:
+        for ens in enseignants:
+            for horaire in horaires:
+                model.Add(sum(emploi_du_temps[(ens, salle, cours, horaire, jour)] for salle in salles for jour in jours) == 1)
+
+    for horaire in horaires:
+        for ens in enseignants:
+            for jour in jours:
+                model.Add(sum(emploi_du_temps[(ens, salle, cours, horaire, jour)] for salle in salles for cours in cours_liste) <= 1)
+
+    # Ajouter la contrainte spécifique aux salles (au plus un cours par salle et par horaire)
+    for salle in salles:
+        for horaire in horaires:
+            for jour in jours:
+                model.Add(sum(emploi_du_temps[(ens, salle, cours, horaire, jour)] for ens in enseignants for cours in cours_liste) <= 1)
+
+    # Résoudre le modèle
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+ 
+    # Imprimer le statut de la résolution
+   # print(solver.ResponseStats())
+
+    # Afficher le message d'incompatibilité si le problème est non réalisable
+    if status == cp_model.INFEASIBLE:
+        print("Le problème est non réalisable. Vérifiez vos contraintes.")
+    else:
+        # Afficher les résultats
+        for ens in enseignants:
+            for salle in salles:
+                for cours in cours_liste:
+                    for horaire in horaires:
+                        for jour in jours:
+                            if solver.Value(emploi_du_temps[(ens, salle, cours, horaire, jour)]) == 1:
+                               print("Enseignant {}, Salle {}, Cours {}, Horaire {}, Jour {}".format(ens, salle, cours, horaire, jour))
+
+#ts = time.time()
+if __name__ == "__main__":
+    enseignants = ['e1', 'e2', 'e3','e4','e5']
+    salles = ['s1', 's2', 's3','s4','s5']
+    cours_liste = ['c1', 'c2', 'c3','c4','c5']
+    horaires = ['h1', 'h2', 'h3','h4','h5']
+    jours = ['Lundi', 'Mardi', 'Mercredi','Jeudi','Vendredi']
+
+    generer_emploi_du_temps(enseignants, salles, cours_liste, horaires, jours)
+#    print("Time required is:", time.time() - ts)
